cdc49e841cd89521b1754fee8491f36d5e698805
ppyporpeem <por_noppavit@hotmail.com>
2016-03-04 18:13:33 +0700

    new yukkuri file

--- PythonApplication4/PythonApplication4.py
+++ PythonApplication4/PythonApplication4.py
@@ -8,30 +8,18 @@ class Simple_drawing_window(QWidget):
     def __init__(self):
         QWidget.__init__(self, None)
         self.setWindowTitle("Simple Drawing")
+        self.wtf = QImage("images/yukkuri-15.jpg")
         self.rabbit = QImage("images/rabbit.png")
 
     def paintEvent(self, e):
         p = QPainter()
+        q = QPainter()
         p.begin(self)
-
-        p.setPen(QColor(0, 0, 0))
-        p.setBrush(QColor(0, 127, 0))
-        p.drawPolygon([
-            QPoint( 70, 100), QPoint(100, 110),
-            QPoint(130, 100), QPoint(100, 150),
-        ])
-
-        p.setPen(QColor(255, 127, 0))
-        p.setBrush(QColor(255, 127, 0))
-        p.drawPie(50, 150, 100, 100, 0, 180 * 16)
-
-        p.drawPolygon([
-            QPoint(50, 200), QPoint(150, 200), QPoint(100, 400),
-        ])
-
         p.drawImage(QRect(200, 100, 320, 320), self.rabbit)
         p.end()
-
+        q.begin(self)
+        q.drawImage(QRect(200+150, 100+150, 320+150, 320+150), self.wtf)
+        q.end()
 def main():
     app = QApplication(sys.argv)
 
